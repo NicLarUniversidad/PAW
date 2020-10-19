13439c8a3ebaf10b39839eda13482a03a7ee522e
NicLarUniversidad <71468011+NicLarUniversidad@users.noreply.github.com>
2020-10-19 20:43:52 -0300

    CSS listaTurnos.html update

---
+++ js/main.js
@@ -0,0 +1,32 @@
+function show(element){
+    if (element){
+        document.getElementById(element).style.visibility= "visible";
+    }
+}
+
+
+function hide(element){
+    if (element){
+        document.getElementById(element).style.visibility= "hidden";
+    }
+}
+
+function ShowFilter(){
+    show("filter");
+    show("filter-container");
+}
+
+function ShowDateFilter(){
+    show("date-filter");
+    show("date-filter-container");
+}
+
+function HideFilter(){
+    hide("filter");
+    hide("filter-container");
+}
+
+function HideDateFilter(){
+    hide("date-filter");
+    hide("date-filter-container");
+}
